adb6cd963e7d86d4fa6186d57422f440974eb861
Justin Berry <jtberry1@gmail.com>
2017-07-05 00:17:37 +1000

    - Gradle build can generate a fat jar of application. - Create simple (and untested..) Main class for application,   tying evaluation & writing of the resulting csv together.

--- src/main/java/berry/justin/spreadsheet/SpreadsheetApp.java
+++ src/main/java/berry/justin/spreadsheet/SpreadsheetApp.java
@@ -1,4 +1,26 @@
 package berry.justin.spreadsheet;
 
+import berry.justin.spreadsheet.io.SpreadsheetWriter;
+
+import java.io.IOException;
+import java.util.List;
+
 public class SpreadsheetApp {
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.err.println("Provide input and output file as arguments.");
+      return;
+    }
+
+    String inputFile = args[0];
+    String outputFile = args[1];
+
+    try {
+      List<String[]> evaluatedSpreadsheet =
+          new SpreadsheetEvaluator(inputFile).evaluate();
+      new SpreadsheetWriter(outputFile).writeAll(evaluatedSpreadsheet);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
